9f24440ecbddec3869570614d761da10720192bc
Nodar <nodar.panchenko@gmail.com>
2023-09-21 14:59:37 +0400

    Доработал планировщик.

--- service/runapscheduler.py
+++ service/runapscheduler.py
@@ -1,18 +1,12 @@
 from apscheduler.schedulers.background import BackgroundScheduler
-from apscheduler.triggers.cron import CronTrigger
 from django.core.management import call_command
 
 
 def mailing_scheduler():
-    call_command('start_mailing')
+    return call_command('start_mailing')
 
 
 def start():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(
-        func=mailing_scheduler,
-        trigger=CronTrigger(second="*/5"),
-        id="mailing_scheduler",
-        max_instances=1,
-    )
+    scheduler.add_job(mailing_scheduler, 'interval', seconds=5)
     scheduler.start()
